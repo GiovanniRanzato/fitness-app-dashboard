db31e82934c787ba621b44e5ddc410de585efea3
Giovanni Ranzato <giovanni.ranzato2010@gmail.com>
2023-10-25 17:24:45 +0200

    Add the side nav

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,17 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import TheSideNav from "./components/TheSideNav.vue"
 </script>
 
 <template>
-    <v-layout>
-        <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
-            <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="John Leider" nav>
-                <template v-slot:append>
-                    <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
-                </template>
-            </v-list-item>
 
-            <v-divider></v-divider>
-
-            <v-list density="compact" nav>
-                <v-list-item prepend-icon="mdi-home-city" title="Users" value="users"></v-list-item>
-                <v-list-item prepend-icon="mdi-account" title="Cards" value="cards"></v-list-item>
-                <v-list-item prepend-icon="mdi-account-group-outline" title="Exercises" value="exercise"></v-list-item>
-            </v-list>
-        </v-navigation-drawer>
+    <v-layout full-height>
+        <TheSideNav />
         <v-main>
-            <v-sheet width="100%" min-height="100vh" class="pa-4 mx-auto">
-                <RouterView />
-            </v-sheet>
-            <v-footer class="bg-grey-lighten-1">
-                <v-row justify="center" no-gutters>
-                    <v-btn v-for="link in links" :key="link" color="white" variant="text" class="mx-2" rounded="xl">
-                        {{ link }}
-                    </v-btn>
-                    <v-col class="text-center mt-4" cols="12">
-                        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-                    </v-col>
-                </v-row>
-            </v-footer>
+          <v-sheet width="100%" min-height="100vh" class="pa-4 mx-auto">
+            <RouterView />
+          </v-sheet>
         </v-main>
     </v-layout>
 </template>
-<script lang="ts">
-export default {
-    data() {
-        return {
-            drawer: true,
-            rail: true,
-            links: ['Termini servizio', 'Privacy policy'],
-        }
-    },
-}
-</script>
 
---
+++ src/components/TheSideNav.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import router from "../router/index"
+const menu = [
+    {
+        icon: "mdi-account-multiple",
+        key: "users",
+        route: "users",
+        label: "Gestione utente"
+    },
+    {
+        icon: "mdi-cards",
+        key: "cards",
+        route: "cards",
+        label: "Schede"
+    },
+    {
+        icon: "mdi-run",
+        key: "exercises",
+        route: "exercises",
+        label: "Esercizi"
+    },
+]
+</script>
+<template>
+    <v-navigation-drawer class="grey-darken-3" theme="dark" permanent>
+        <v-list color="transparent">
+            <v-list-item v-for="item in menu" 
+            :prepend-icon="item.icon" 
+            :title="item.label" 
+            :key="item.key"
+            @click="router.push(item.route)"
+            >
+        </v-list-item>
+        </v-list>
+
+        <template v-slot:append>
+            <div class="pa-2">
+                <v-btn block>
+                    Logout
+                </v-btn>
+            </div>
+        </template>
+    </v-navigation-drawer>
+</template>
