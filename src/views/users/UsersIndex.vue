0d2bf2939b64edbf8b5008aefed7804c187f0c83
Giovanni Ranzato <giovanni.ranzato2010@gmail.com>
2023-11-19 18:57:25 +0100

    Add create User controll

--- src/views/users/UsersIndex.vue
+++ src/views/users/UsersIndex.vue
@@ -4,6 +4,9 @@ import BasePageTitle from './../../components/base/BasePageTitle.vue'
 import BasePageActions from './../../components/base/BasePageActions.vue'
 import TheUsersTable from './TheUsersTable.vue'
 import router from "../../router/index"
+import { useAuthStore } from '../../stores/auth'
+
+const authStore = useAuthStore()
 
 const addItem = () => {
   router.push('users/add')
@@ -14,7 +17,7 @@ const addItem = () => {
   <main>
     <BaseContainer>
       <BasePageTitle title="Utenti" />
-      <BasePageActions :onAdd="addItem"/>
+      <BasePageActions v-if="authStore.canCreateUser" :onAdd="addItem"/>
       <TheUsersTable />
     </BaseContainer>
   </main>
