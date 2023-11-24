721bd6be8d16e7931c8d6dbdbfc1072a2994d9ad
Giovanni Ranzato <giovanni.ranzato2010@gmail.com>
2023-11-24 19:45:14 +0100

    Fix BR

--- src/views/exercises/ExerciseView.vue
+++ src/views/exercises/ExerciseView.vue
@@ -15,21 +15,25 @@ const props = defineProps({
 })
 const exercisesStore = useExercisesStore()
 const exerciseAttributesValues = exercisesStore.getExerciseAttributesValuesById(parseInt(props.id))
+
+const formatText = (text: string) => {
+    return text.replace(/\n/g, "<br>");
+}
 </script>
 <template>
     <BaseContainer>
         <BasePageTitle title="Visualizza esercizio" />
         <template v-if="exerciseAttributesValues">
 
-                    <BaseCard :title="exerciseAttributesValues.name ">
-                        <v-card-item>{{ exerciseAttributesValues.description }}</v-card-item>
-                        <v-card-item v-if=" exerciseAttributesValues.notes ">
-                            <BaseAlertInfo>{{ exerciseAttributesValues.notes }}</BaseAlertInfo>
-                        </v-card-item>
-                        <v-card-item  v-if="exerciseAttributesValues.mediaUrl">
-                            <BaseYouTubeVideoIFrame :mediaUrl="exerciseAttributesValues.mediaUrl" />
-                        </v-card-item>
-                    </BaseCard>
+            <BaseCard :title="exerciseAttributesValues.name">
+                <v-card-item v-html="formatText(exerciseAttributesValues.description)" />
+                <v-card-item v-if="exerciseAttributesValues.notes">
+                    <BaseAlertInfo><div v-html="formatText(exerciseAttributesValues.notes)"></div></BaseAlertInfo>
+                </v-card-item>
+                <v-card-item v-if="exerciseAttributesValues.mediaUrl">
+                    <BaseYouTubeVideoIFrame :mediaUrl="exerciseAttributesValues.mediaUrl" />
+                </v-card-item>
+            </BaseCard>
 
         </template>
         <BaseAlertWarning v-else>Si è verificato un problema: dati esercizio non presenti.</BaseAlertWarning>
